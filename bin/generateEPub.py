b6fc81e5ce082333587d3ec43481e075258e9bc9
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-05-26 15:16:00 -0700

    Started working on epub stuff.

---
+++ bin/generateEPub.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from scriptlib import epub
+
+
+# XXX add command line option parsing
+epub.generate_epub(dst, html_file="", clean_up=False)
---
+++ scriptlib/epub.py
@@ -0,0 +1,9 @@
+import os
+
+
+def build_skeleton(path):
+    pass
+
+
+def generate_epub(dest_path, html_file, clean_up=True):
+    pass
